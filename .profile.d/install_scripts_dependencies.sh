70bc201a77cd10a685637bb1e8572e1d6bd31c34
Minach <ignaciomedinar@gmail.com>
2023-11-10 19:49:39 +0100

    Other try

--- .profile.d/install_scripts_dependencies.sh
+++ .profile.d/install_scripts_dependencies.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-# Create a virtual environment
-python -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
 # Use python -m pip to install script dependencies
 python -m pip install -r requirements_scripts.txt
-
-# Deactivate the virtual environment
-deactivate

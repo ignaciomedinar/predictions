81046fc1440b36d775b348d64f4969a2874a310f
Minach <ignaciomedinar@gmail.com>
2023-11-11 13:12:09 +0100

    Change to other test

--- .profile.d/install_scripts_dependencies.sh
+++ .profile.d/install_scripts_dependencies.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# Use python -m pip to install script dependencies
-python -m pip install -r requirements_scripts.txt
+# Navigate to the app directory
+cd /app
+
+# Use pip to install script dependencies
+pip install -r requirements_scripts.txt

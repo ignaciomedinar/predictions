8bf240f2f4ba04aaad39aebad5e7d658d73a892b
Minach <ignaciomedinar@gmail.com>
2024-03-28 22:41:04 +0100

    try to fix

--- querying.py
+++ querying.py
@@ -9,9 +9,18 @@ cnx = mysql.connector.connect(
 )
 cursor = cnx.cursor()
 
-update_flag = ("SELECT * "
-               "FROM heroku_9f69e70d94a5650.predictions_history "
-               "LIMIT 10 "
+update_flag = ("SELECT distinct fr.*, ph.bet, case when ph.bet is null or fr.goalslocal ='' "
+                    "then 'NA' when upper(fr.Result)=upper(left(ph.bet,1)) "
+                    "then 'Correct' else 'Incorrect' end as success, fl.flag_url, "
+                    "ph.phg, ph.pag, ph.max_prob "
+                    "FROM heroku_9f69e70d94a5650.football_results fr "
+                    "left join heroku_9f69e70d94a5650.predictions_history ph "
+                    "on fr.date = ph.date and fr.local=ph.local and fr.visitor=ph.visitor "
+                    "left join heroku_9f69e70d94a5650.flags fl "
+                    "on upper(fl.Country) = upper(fr.League) "
+                    "AND fr.goalslocal <>'' "
+                    "ORDER BY fr.date desc, fr.League "
+                    "limit 10"
 )
 cursor.execute(update_flag)
 
